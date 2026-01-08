c058f8a938103d799263caf677829e0994c1e3b7
KevSky21 <105402578+KevSky21@users.noreply.github.com>
2026-01-08 11:31:10 -0600

    Add Item class with attributes and methods
    This class is the item class which has simple properties to display from the inventory class

---
+++ Lists and Dicts/item_class.py
@@ -0,0 +1,14 @@
+class Item:
+    def __init__(self, name:str, quantity:int, value:float):
+        self.name = name
+        self.quantity = quantity
+        self.value = value
+
+    def __str__(self):
+        return f"{self.name} (Qty: {self.quantity}, Value: ${self.value})"
+
+    def print_item(self):
+        print(self.name)
+        print(self.quantity)
+        print(self.value)
+
